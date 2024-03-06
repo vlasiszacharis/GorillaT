b1c71d375008d7312bdc17593dcf7086aaa410a0
vlasiszacharis <vlasisz983@gmail.com>
2024-03-06 13:42:55 +0200

    baseUrl

--- src/utils/config/BaseUrl.ts
+++ src/utils/config/BaseUrl.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8080";
-//export const BASE_URL = "https://urchin-app-izw4q.ondigitalocean.app";
+//export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://urchin-app-izw4q.ondigitalocean.app";
